c7701564f50814fa4168e4e5f36f72dc98cf80d7
Scott <scott@paragonie.com>
2016-05-29 13:19:46 -0400

    Motif installer

--- src/Engine/Continuum/Installers/Motif.php
+++ src/Engine/Continuum/Installers/Motif.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace Airship\Engine\Continuum\Installers;
 
 use \Airship\Engine\Continuum\Installer as BaseInstaller;
+use \Psr\Log\LogLevel;
 
 /**
  * Class Motif
@@ -15,6 +16,39 @@ class Motif extends BaseInstaller
 {
     protected $type = 'Motif';
     protected $ext = 'zip';
+
+    /**
+     * Add the new motif as an option to the Cabin
+     *
+     * @param string $cabin
+     * @return bool
+     */
+    protected function addMotifToCabin(string $cabin): bool
+    {
+        if (!\is_dir(ROOT . '/Cabins/' . $cabin)) {
+            return false;
+        }
+        $supplier = $this->supplier->getName();
+        $name = $this->package;
+
+        $configFile = ROOT . '/Cabins/' . $cabin . '/config/motifs.json';
+        $config = \Airship\loadJSON($configFile);
+
+        $i = 1;
+        while (isset($config[$name])) {
+            $name = $this->package . '-' . ++$i;
+        }
+
+        $config[$name] = [
+            'path' =>
+                $supplier . '/' . $this->package
+        ];
+
+        return \file_put_contents(
+            $configFile,
+            \json_encode($config, JSON_PRETTY_PRINT)
+        ) !== false;
+    }
 
     /**
      * Motif install process.
@@ -30,6 +64,74 @@ class Motif extends BaseInstaller
      */
     public function install(InstallFile $fileInfo): bool
     {
+        $path = $fileInfo->getPath();
+        $zip = new \ZipArchive();
+        $res = $zip->open($path);
+        if ($res !== true) {
+            $this->log(
+                'Could not open the ZipArchive.',
+                LogLevel::ERROR
+            );
+            return false;
+        }
 
+        // Extraction destination directory
+        $dir = \implode(
+            DIRECTORY_SEPARATOR,
+            [
+                ROOT,
+                'Motifs',
+                $this->supplier->getName(),
+                $this->package
+            ]
+        );
+        if (!\is_dir($dir)) {
+            \mkdir($dir, 0775, true);
+        }
+
+        // Grab metadata
+        $metadata = \Airship\loadJSON(
+            $zip->getArchiveComment(\ZipArchive::FL_UNCHANGED)
+        );
+        if (isset($metadata['cabin'])) {
+            $cabin = \trim(
+                \preg_replace('/[^A-Za-z0-9\_]/', '_', $metadata['cabin']),
+                '_'
+            );
+            if (!\is_dir(ROOT . '/Cabins/' . $cabin)) {
+                $this->log(
+                    'Could not install; cabin "' . $cabin . '" is not installed.',
+                    LogLevel::ERROR
+                );
+                return false;
+            }
+        } else {
+            $cabin = null;
+        }
+
+        // Extract the new files to the current directory
+        if (!$zip->extractTo($dir)) {
+            $this->log(
+                'Could not extract Motif to its destination.',
+                LogLevel::ERROR
+            );
+            return false;
+        }
+
+        // Add to the relevant motifs.json files
+        if ($cabin) {
+            $this->addMotifToCabin($cabin);
+        } else {
+            foreach (\glob(ROOT . '/Cabins/') as $cabin) {
+                if (\is_dir($cabin)) {
+                    $this->addMotifToCabin(
+                        \Airship\path_to_filename($cabin)
+                    );
+                }
+            }
+        }
+
+        // Finally, nuke the cache:
+        return $this->clearCache();
     }
 }
