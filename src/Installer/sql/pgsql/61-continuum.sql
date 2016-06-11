d1fe65d2210462d1d36a0f7bbc1098e058901273
Paragon Initiative Enterprises <security@paragonie.com>
2016-06-10 20:50:42 -0400

    Use two tables for package update tracking

--- src/Installer/sql/pgsql/61-continuum.sql
+++ src/Installer/sql/pgsql/61-continuum.sql
@@ -1,24 +1,28 @@
-CREATE TABLE airship_package_updates (
-    updateid BIGSERIAL PRIMARY KEY,
+CREATE TYPE type_airship_package AS ENUM ('Core', 'Cabin', 'Gadget', 'Motif');
+
+CREATE TABLE airship_package_cache (
+    packageid BIGSERIAL PRIMARY KEY,
+    packagetype type_airship_package,
     supplier TEXT,
     name TEXT,
-    type TEXT,
+    created TIMESTAMP DEFAULT NOW(),
+    modified TIMESTAMP DEFAULT NOW()
+);
+CREATE INDEX ON airship_package_cache (packagetype);
+CREATE INDEX ON airship_package_cache (supplier);
+CREATE INDEX ON airship_package_cache (name);
+CREATE UNIQUE INDEX ON airship_package_cache(packagetype, supplier, name);
+
+CREATE TABLE airship_package_versions (
+    versionid BIGSERIAL PRIMARY KEY,
+    package BIGINT REFERENCES airship_package_cache(packageid),
     version TEXT,
     checksum TEXT,
-    treeupdate BIGINT REFERENCES airship_tree_updates (treeupdateid),
+    treeupdateid BIGINT REFERENCES airship_tree_updates(treeupdateid),
     created TIMESTAMP DEFAULT NOW(),
     modified TIMESTAMP DEFAULT NOW()
 );
 
-CREATE INDEX ON airship_package_updates (name);
-CREATE INDEX ON airship_package_updates (supplier);
-CREATE INDEX ON airship_package_updates (type);
-CREATE INDEX ON airship_package_updates (checksum);
-CREATE INDEX ON airship_package_updates (version);
-CREATE INDEX ON airship_package_updates (type, supplier, name);
-CREATE INDEX ON airship_package_updates (type, supplier, name, version);
-
-DROP TRIGGER IF EXISTS update_airship_package_updates_modtime ON airship_package_updates;
-CREATE TRIGGER update_airship_package_updates_modtime
-  BEFORE UPDATE ON airship_package_updates
-  FOR EACH ROW EXECUTE PROCEDURE update_modtime();
+CREATE INDEX ON airship_package_versions (version);
+CREATE INDEX ON airship_package_versions (checksum);
+CREATE UNIQUE INDEX ON airship_package_versions (package, version);
