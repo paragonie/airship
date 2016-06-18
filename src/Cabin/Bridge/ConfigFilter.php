bd7fec4923b0941b7f7defc16b5d30b0e345c278
Paragon Initiative Enterprises <security@paragonie.com>
2016-06-17 20:18:45 -0400

    Add filters for 2FA config

--- src/Cabin/Bridge/ConfigFilter.php
+++ src/Cabin/Bridge/ConfigFilter.php
@@ -30,6 +30,32 @@ class ConfigFilter extends InputFilterContainer
             ->addFilter('config_extra.password-reset.enabled', new BoolFilter())
             ->addFilter('config_extra.password-reset.ttl', new IntFilter())
             ->addFilter('config_extra.file.cache', new IntFilter())
+            ->addFilter('config_extra.two-factor.label', new StringFilter())
+            ->addFilter('config_extra.two-factor.issuer', new StringFilter())
+            ->addFilter(
+                'config_extra.two-factor.length',
+                (new IntFilter())->addCallback(
+                    function ($var): int {
+                        if ($var < 6) {
+                            return 6;
+                        } elseif ($var > 8) {
+                            return 8;
+                        }
+                        return (int) $var;
+                    }
+                )
+            )
+            ->addFilter(
+                'config_extra.two-factor.period',
+                (new IntFilter())->addCallback(
+                    function ($var): int {
+                        if ($var < 30) {
+                            return 30;
+                        }
+                        return (int) $var;
+                    }
+                )
+            )
             /* twig_vars */
             ->addFilter('twig_vars.active-motif', new StringFilter())
             ->addFilter('twig_vars.title', new StringFilter())
