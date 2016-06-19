5811a69c2178fbd51f50137070b78046b86e0bee
Paragon Initiative Enterprises <security@paragonie.com>
2016-06-19 04:14:10 -0400

    Fixed audit_helper.php

--- tools/audit_helper.php
+++ tools/audit_helper.php
@@ -15,23 +15,35 @@ $repository = 'https://github.com/paragonie/airship/blob/master/';
 $cutoff = \strlen(\dirname(__DIR__) . '/src') + 1;
 $dirs = [];
 
-foreach ($extensions as $ex) {
-    foreach (\Airship\list_all_files(\dirname(__DIR__) . '/src', $ex) as $file) {
-        $print = \trim(\substr($file, $cutoff), '/');
+$allDirs = \Airship\list_all_files(\dirname(__DIR__) . '/src');
+\sort($allDirs);
+foreach ($allDirs as $file) {
+    $print = \trim(\substr($file, $cutoff), '/');
 
-        $pieces = \explode('/', $print);
-        $max = \count($pieces) - 1;
-        $name = \array_pop($pieces);
-        $currentDir = \implode('/', $pieces);
-        if ($max > 0 && !isset($dirs[$currentDir])) {
-            echo \str_repeat(' ', 4 * ($max - 1)) . '- [ ] ';
-            echo $pieces[$max - 1];
-            $dirs[$currentDir] = true;
-            echo "\n";
-        }
+    $pieces = \explode('/', $print);
+    $max = \count($pieces) - 1;
+    $name = \array_pop($pieces);
 
-        echo \str_repeat(' ', 4 * $max) . '- [ ] ';
-        echo '[' . $name . '](' . $repository . $print . ')';
+    if (\substr($print, 0, 3) === 'tmp' || \substr($print, 0, 5) === 'files') {
+        continue;
+    }
+
+    $currentDir = \implode('/', $pieces);
+    if ($max > 0 && !isset($dirs[$currentDir])) {
+        echo \str_repeat(' ', 3 * ($max - 1)) . '- [ ] ';
+        echo $pieces[$max - 1];
+        $dirs[$currentDir] = true;
         echo "\n";
     }
+    if (\preg_match('/\.([^.]+)$/', $name, $m)) {
+        if (!\in_array($m[1], $extensions)) {
+            continue;
+        }
+    } else {
+        continue;
+    }
+
+    echo \str_repeat(' ', 3 * $max) . '- [ ] ';
+    echo '[' . $name . '](' . $repository . $print . ')';
+    echo "\n";
 }
