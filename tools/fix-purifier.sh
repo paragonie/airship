0bd53576e6ac8ad53797023755d035c7f217faa5
Paragon Initiative Enterprises <security@paragonie.com>
2016-06-20 14:37:56 -0400

    Less risky permissions with HTMLPurifier

--- tools/fix-purifier.sh
+++ tools/fix-purifier.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-chmod -R 0777 ../vendor
+chmod -R 0775 ../vendor/ezyang/htmlpurifier/library/HTMLPurifier/DefinitionCache/Serializer
 
 # Make the Serializer Cache errors go away forever.
