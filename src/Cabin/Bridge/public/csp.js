8204b05b18d386fd21e9d0d8cade34083d85c268
Paragon Initiative Enterprises <security@paragonie.com>
2016-06-26 21:47:40 -0400

    Found a bug!
    The Twig selector for the CSP editing form wasn't properly checking for "-src" to display the data URI checkbox.
    
    Like many of the bugs I've found in my internal audits, this caused it to fail closed. Not a security bug, but a usability bug for sure.

--- src/Cabin/Bridge/public/csp.js
+++ src/Cabin/Bridge/public/csp.js
@@ -1,32 +1,37 @@
-$(".csp_add_btn").click(function() {
-    var key = $(this).data('key');
-    if (key === 'plugin-types') {
-        var placeholder = 'application/javascript';
-    } else {
-        var placeholder = 'example.com';
-    }
-    $("#csp_"+key+"_whitelist").append(
-        "<li><input \n"+
-        "        class=\"full-width\"\n"+
-        "        type=\"text\"\n"+
-        "        placeholder=\""+placeholder+"\"\n"+
-        "        name=\"content_security_policy["+key+"][allow][]\"\n"+
-        "    /></li>"
-    );
-});
-$('.csp_disable_all').each(function() {
-    var key = $(this).data('key');
-    if ($(this).is(':checked')) {
-        $("#csp_"+key+"_inner").hide('fast');
-    } else {
-        $("#csp_"+key+"_inner").show('fast');
-    }
-});
-$('.csp_disable_all').click(function() {
-    var key = $(this).data('key');
-    if ($(this).is(':checked')) {
-        $("#csp_"+key+"_inner").hide('fast');
-    } else {
-        $("#csp_"+key+"_inner").show('fast');
-    }
+$(document).ready(function() {
+    $(".csp_add_btn").click(function () {
+        var key = $(this).data('key');
+        if (key === 'plugin-types') {
+            var placeholder = 'application/javascript';
+        } else {
+            var placeholder = 'example.com';
+        }
+        $("#csp_" + key + "_whitelist").append(
+            "<li><input \n" +
+            "        class=\"full-width\"\n" +
+            "        title=\"Whitelist\"\n" +
+            "        type=\"text\"\n" +
+            "        placeholder=\"" + placeholder + "\"\n" +
+            "        name=\"content_security_policy[" + key + "][allow][]\"\n" +
+            "    /></li>"
+        );
+    });
+
+    $('.csp_disable_all').each(function () {
+        var key = $(this).data('key');
+        if ($(this).is(':checked')) {
+            $("#csp_" + key + "_inner").hide('fast');
+        } else {
+            $("#csp_" + key + "_inner").show('fast');
+        }
+    });
+
+    $('.csp_disable_all').click(function () {
+        var key = $(this).data('key');
+        if ($(this).is(':checked')) {
+            $("#csp_" + key + "_inner").hide('fast');
+        } else {
+            $("#csp_" + key + "_inner").show('fast');
+        }
+    });
 });
