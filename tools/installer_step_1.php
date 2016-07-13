066787bfb58ca59bb8bd80c990c243244fd5e2c4
Paragon Initiative Enterprises <security@paragonie.com>
2016-07-13 17:02:36 -0400

    This needs to work even if nothing is installed.

--- tools/installer_step_1.php
+++ tools/installer_step_1.php
@@ -7,27 +7,30 @@ use \ParagonIE\ConstantTime\Base64UrlSafe;
  * Used in automated deployment scripts. Generates, stores, then echos a random
  * PostgreSQL password, then sets up the web-based installer for step 2.
  */
-require_once \dirname(__DIR__) . '/src/bootstrap.php';
+require_once \dirname(__DIR__) . '/vendor/autoload.php';
 
 $password = Base64UrlSafe::encode(\random_bytes(33));
 
-\Airship\saveJSON(
+\file_put_contents(
     \dirname(__DIR__) . '/src/tmp/installing.json',
-    [
-        'step' => 2,
-        'database' => [
-            [
+    \json_encode(
+        [
+            'step' => 2,
+            'database' => [
                 [
-                    'driver' => 'pgsql',
-                    'host' => $argv[1] ?? 'localhost',
-                    'port' => $argv[2] ?? 5432,
-                    'database' => 'airship',
-                    'username' => 'airship',
-                    'password' => $password
+                    [
+                        'driver' => 'pgsql',
+                        'host' => $argv[1] ?? 'localhost',
+                        'port' => $argv[2] ?? 5432,
+                        'database' => 'airship',
+                        'username' => 'airship',
+                        'password' => $password
+                    ]
                 ]
             ]
-        ]
-    ]
+        ],
+        JSON_PRETTY_PRINT
+    )
 );
 
 echo $password;
