6d669fc8aec6af5ad0fde96ee01bfb54cde51b95
Paragon Initiative Enterprises <security@paragonie.com>
2016-08-28 09:53:00 -0400

    Nuke symlinks on upgrade.

--- src/Updates/1.3.0/symlinks.php
+++ src/Updates/1.3.0/symlinks.php
@@ -1,9 +1,22 @@
 <?php
 declare(strict_types=1);
 
+use Airship\Engine\State;
+
 /**
  * This script runs when upgrading to v1.3.0 from an earlier version.
  * It deletes the old symlinks used for resolving Motif templates.
- *
- * @todo Finish this before releasing v1.3.0
+ * The bootstrapping process is sufficient to restore them.
  */
+
+$state = State::instance();
+
+foreach ($state->cabins as $cabin) {
+    $cabinName = (string) ($cabin['namespace'] ?? $cabin['name']);
+    foreach (\glob(ROOT . '/Cabin/' . $cabinName . '/Lens/motif/*') as $f) {
+        $endPiece = \Airship\path_to_filename($f);
+        if (\is_link($f)) {
+            \unlink($f);
+        }
+    }
+}
